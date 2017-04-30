c434137cdccf072aa193a212aab64b3d36f009eb
avinash patnala <avinashpatnala@avinashs-MacBook-Air.local>
2017-05-01 03:04:32 +0530

    callbacks

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,6 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def trakt
+    #You need to implement the method below in your model (e.g. app/models/user.rb)
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
 
 
   get   '/login', :to => 'sessions#new', :as => :login
-match '/auth/verify', :to => 'sessions#create',:via => [:get,:post]
+#match '/auth/verify', :to => 'sessions#create',:via => [:get,:post]
+devise_for :users, controllers: { omniauth_callbacks:  'omniauth_callbacks' }
 match '/auth/failure', :to => 'sessions#failure',:via => [:get,:post]
 
 match 'pages/oAuth' => 'pages#oAuth', :as => :newbill, :via => [:get]
-  devise_for :users
+  
   get 'pages/index'
 
 
